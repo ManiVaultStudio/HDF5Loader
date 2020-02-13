7bf6e1061b0e3ea304c4e8d38188dd67476f5ef4
Niels Dekker <N.Dekker@lumc.nl>
2020-02-13 15:48:04 +0100

    Replace old string-based by new functor-based Qt signal slot connection
    Uses modern (Qt 5) functor-based Qt signal-slot connection when connecting to `_openFileDialog`, as a preparation to fix the memory leak of this dialog.
    
    See also:
     * https://doc.qt.io/qt-5/signalsandslots-syntaxes.html
     * https://wiki.qt.io/New_Signal_Slot_Syntax

--- src/HDF5Loader.h
+++ src/HDF5Loader.h
@@ -19,7 +19,6 @@ class QLabel;
 
 class HDF5Loader : public QObject, public LoaderPlugin
 {
-	Q_OBJECT
 public:
     HDF5Loader();
     ~HDF5Loader(void) override;
@@ -28,11 +27,6 @@ public:
 
     void loadData() Q_DECL_OVERRIDE;
 
-
-
-private slots:
-	void nameFilterChanged(int);
-	void nameFilterChanged(const QString &);
 private:
     QString _fileName;
 	QFileDialog *_openFileDialog;
