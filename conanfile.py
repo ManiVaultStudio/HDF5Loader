932c1e66786ade12a81c63cc39572283c3daa391
Alexander Vieth <vieth.alexander@gmx.net>
2026-07-24 10:34:23 +0200

    CI: Remove Release build and install steps

--- conanfile.py
+++ conanfile.py
@@ -132,7 +132,6 @@ class HDF5LoaderConan(ConanFile):
 
         cmake = self._configure_cmake()
         cmake.build(build_type="RelWithDebInfo")
-        cmake.build(build_type="Release")
 
     def package(self):
         package_dir = pathlib.Path(self.build_folder, "package")
@@ -148,17 +147,6 @@ class HDF5LoaderConan(ConanFile):
                 "RelWithDebInfo",
                 "--prefix",
                 relWithDebInfo_dir,
-            ]
-        )
-        subprocess.run(
-            [
-                "cmake",
-                "--install",
-                self.build_folder,
-                "--config",
-                "Release",
-                "--prefix",
-                release_dir,
             ]
         )
 
@@ -180,6 +168,3 @@ class HDF5LoaderConan(ConanFile):
         self.cpp_info.relwithdebinfo.libdirs = ["RelWithDebInfo/lib"]
         self.cpp_info.relwithdebinfo.bindirs = ["RelWithDebInfo/Plugins", "RelWithDebInfo"]
         self.cpp_info.relwithdebinfo.includedirs = ["RelWithDebInfo/include", "RelWithDebInfo"]
-        self.cpp_info.release.libdirs = ["Release/lib"]
-        self.cpp_info.release.bindirs = ["Release/Plugins", "Release"]
-        self.cpp_info.release.includedirs = ["Release/include", "Release"]
